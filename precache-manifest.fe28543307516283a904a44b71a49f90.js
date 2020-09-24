4cdcb4949d4c954123a8eb0f655edaa47531966f
TrungChien2201 <chienvu.neon@gmail.com>
2020-09-24 14:10:08 +0700

    Updates

---
+++ precache-manifest.fe28543307516283a904a44b71a49f90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b1bc564fb825c9399ecb47812c9128e",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "fea29a317c9360a182a6",
+    "url": "/todo/static/css/main.5be2b058.chunk.css"
+  },
+  {
+    "revision": "aec7cfeed17a552b9587",
+    "url": "/todo/static/js/2.93a009af.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/todo/static/js/2.93a009af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fea29a317c9360a182a6",
+    "url": "/todo/static/js/main.07c22f27.chunk.js"
+  },
+  {
+    "revision": "da7c3f7f7e5963fe74b8",
+    "url": "/todo/static/js/runtime-main.74315f96.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todo/static/media/logo.5d5d9eef.svg"
+  }
+]);
